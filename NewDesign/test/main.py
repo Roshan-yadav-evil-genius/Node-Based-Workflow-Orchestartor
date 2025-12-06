ad9e4fca03767d4991431ffc5208c8a99ffe1fb2
RoshanYadav <roshan.yadav@12thwonder.com>
2025-12-06 22:55:04 +0530

    refactor: Remove deprecated functions from FormSerializer
    - Eliminated obsolete backward compatibility functions from FormSerializer, streamlining the code and encouraging the use of updated methods.
    - Updated main.py to include a validation check for the form, enhancing testing capabilities.

--- NewDesign/test/main.py
+++ NewDesign/test/main.py
@@ -22,3 +22,5 @@ if __name__ == "__main__":
 
     form.update_field('language', 'marathi1')  # Populates languages
     print(FormSerializer(form).to_json(),end="-"*20)
+
+    print(form.is_valid())
